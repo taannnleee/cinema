fe7727a3ad65f83c047773bdba05872b32d88416
taannnleee <leetaan1902@gmail.com>
2023-12-31 20:17:31 +0700

    stratery

--- src/main/java/com/example/cinema/CinemaApplication.java
+++ src/main/java/com/example/cinema/CinemaApplication.java
@@ -1,5 +1,7 @@
 package com.example.cinema;
 
+import com.example.cinema.StrategyDeSignPattern.Discount50Percent;
+import com.example.cinema.StrategyDeSignPattern.Ticket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,10 @@ public class CinemaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CinemaApplication.class, args);
+        Ticket ticket = new Ticket();
+        ticket.setPrice(100.0);
+        ticket.setPromoteStrategy(new Discount50Percent());
+        System.out.println(ticket.getPromoteStrategy().DoDiscount(ticket.getPrice()));
     }
 
 }
