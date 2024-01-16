99b419aef27e046835b8c03f598ee38fe952bb65
taannnleee <leetaan1902@gmail.com>
2024-01-16 18:24:54 +0700

    no message

--- src/main/java/com/example/cinema/controller/UserController.java
+++ src/main/java/com/example/cinema/controller/UserController.java
@@ -3,25 +3,22 @@ package com.example.cinema.controller;
 import com.example.cinema.entity.User;
 import com.example.cinema.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    @Autowired
-    private UserService userservice;
-//    @GetMapping("")
-//    public ResponseEntity<?> addUser(){
-//        userservice.addUser()
-//        ArrayList<User> users = userservice.addUser();
-//        return  ResponseEntity.ok(users);
-//    }
+    //    @Autowired
+//    private UserService userservice;
+    private final UserService userservice;
+
+    public UserController(UserService userservice) {
+        this.userservice = userservice;
+    }
 
     @PostMapping("/hi")
-    public User add (@RequestBody User user){
+    public User add(@RequestBody User user) {
         return userservice.addUser(user);
     }
 
